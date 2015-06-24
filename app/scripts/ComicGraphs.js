4e099334e562c0c4ba31ec0570640ffeb1ca8253
Jamal Martin <jamalomartin@gmail.com>
2015-06-24 00:17:01 -0600

    Charts and styling
    Added two new charts(line and pie).

--- app/scripts/ComicGraphs.js
+++ app/scripts/ComicGraphs.js
@@ -2,12 +2,19 @@
 
 var React = require('react');
 var BarChart = require('react-chartjs').Bar;
+var LineChart = require('react-chartjs').Line;
+var PieChart = require('react-chartjs').Pie;
 
 
 
 var ComicGraphs = React.createClass({
   getInitialState: function() {
-    return {comicPublisher: []};
+    return {
+            comicPublisher: [],
+            comicDate: [],
+            comicTitle: [],
+            comcicTitleCounts: []
+           };
   },
 
   fetchState: function() {
@@ -15,15 +22,26 @@ var ComicGraphs = React.createClass({
     var self = this;
 
     $.getJSON(url, function(result) {
-      var publishers = [];
       var counts = {};
       var count = [];
+      var date = [];
+      var publishers = [];
+      var title = [];
+      var titleCounts = {};
+      var titleCount = [];
 
       result.forEach(function(comicObj, i) {
         if (publishers.indexOf(comicObj.publisher) === -1) {
           publishers.push(comicObj.publisher);
         }
+        if (date.indexOf(comicObj.date) === -1) {
+          date.push(comicObj.date);
+        }
+        if (title.indexOf(comicObj.title) === -1) {
+          title.push(comicObj.title);
+        }
         counts[comicObj.publisher] = (counts[comicObj.publisher] || 0)+1;
+        titleCounts[comicObj.title] = (titleCounts[comicObj.title] || 0)+1;
       });
 
       for (var c in counts) {
@@ -32,8 +50,19 @@ var ComicGraphs = React.createClass({
         }
       }
 
-      self.setState({comicPublisher: publishers});
-      self.setState({publisherCounts: count});
+      for (var t in titleCounts) {
+        if (titleCounts.hasOwnProperty(t)) {
+          titleCount.push(titleCounts[t]);
+        }
+      }
+
+      self.setState({
+                      comicPublisher: publishers,
+                      publisherCounts: count,
+                      comicDate: date,
+                      comicTitle: title,
+                      comcicTitleCounts: titleCount
+                    });
     });
   },
 
@@ -44,17 +73,35 @@ var ComicGraphs = React.createClass({
   render: function() {
     var publisherData = this.state.comicPublisher;
     var publisherCounts = this.state.publisherCounts;
+    var dates = this.state.comicDate;
+    var titles = this.state.comicTitle;
+    var titleCounts = this.state.comcicTitleCounts;
 
     if (publisherData.length >= 0 && publisherCounts !== undefined) {
-      var data = {
+      var barData = {
           labels: publisherData,
           datasets: [
               {
-                label: 'test1',
-                fillColor: 'rgba(220,220,220,0.5)',
-                strokeColor: 'rgba(220,220,220,0.8)',
-                highlightFill: 'rgba(220,220,220,0.75)',
-                highlightStroke: 'rgba(220,220,220,1)',
+                label: 'Number of comics by publisher',
+                fillColor: ' rgba(215,40,40,0.5)',
+                strokeColor: 'rgba(215,40,40,0.8)',
+                highlightFill: 'rgba(215,40,40,0.75)',
+                highlightStroke: 'rgba(215,40,40,1)',
+                data: publisherCounts
+              }
+          ]
+      };
+      var lineData = {
+          labels: dates,
+          datasets: [
+              {
+                label: 'Number of comics by date',
+                fillColor: ' rgba(215,40,40,0.2)',
+                strokeColor: 'rgba(215,40,40,1)',
+                pointColor: 'rgba(215,40,40,1)',
+                pointStrokeColor: '#fff',
+                pointHighlightFill: '#fff',
+                pointHighlightStroke: 'rgba(220,220,220,1)',
                 data: publisherCounts
               }
           ]
@@ -63,13 +110,34 @@ var ComicGraphs = React.createClass({
       return null;
     }
 
+    var data = this.state.comcicTitleCounts.map(function(count, i) {
+      var color = '#'+Math.floor(Math.random()*16777215).toString(16);
+      return (
+            {
+              value: count,
+              color: color,
+              highlight: color,
+              label: titles[i]
+            }
+      );
+    }.bind(this));
     return (
       <div>
-            <BarChart data={data}
+            <BarChart data={barData}
                       width={400}
                       height={400}
-                      className="chart-backgroung-color"
-                      />
+                      className="chart-background-color"
+            />
+          <LineChart data={lineData}
+                      width={400}
+                      height={400}
+                      className="chart-background-color line-chart"
+          />
+        <PieChart data={data}
+                      width={400}
+                      height={400}
+                      className="chart-background-color"
+          />
       </div>
     );
   }
